2454b699b4312d2757e7264a6dff4226e065d9c5
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-05 10:41:49 -0300

    style: align logo in center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="w-full">
       <div className="flex w-full h-52 bg-gray-700 justify-center items-center">
-        <h1 className="text-5xl">
+        <h1 className="text-5xl pb-7">
           <span className="text-blue font-black">to</span>
           <span className="text-purple-dark font-black">do</span>
         </h1>
