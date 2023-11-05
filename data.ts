816e14b90e0c7079a616aabf9190c2fff38d1b26
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-05 10:51:02 -0300

    feat: created Data services and mock data

---
+++ data.ts
@@ -0,0 +1,20 @@
+export interface Task {
+  id: number;
+  description: string;
+  isCompleted: boolean;
+}
+
+const tasks: Task[] = [
+  { id: 1, description: 'Fazer compras', isCompleted: false },
+  { id: 2, description: 'Estudar TypeScript', isCompleted: false },
+  { id: 3, description: 'Limpar o quarto', isCompleted: false },
+  { id: 4, description: 'Fazer exercícios', isCompleted: false },
+  { id: 5, description: 'Assistir a um filme', isCompleted: false },
+  { id: 6, description: 'Ligar para um amigo', isCompleted: false },
+  { id: 7, description: 'Preparar o jantar', isCompleted: false },
+  { id: 8, description: 'Criar novo projeto', isCompleted: false },
+  { id: 9, description: 'Ler um livro', isCompleted: false },
+  { id: 10, description: 'Escrever código', isCompleted: false },
+]
+
+export default tasks
