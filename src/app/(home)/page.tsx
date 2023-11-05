2d3257a60b66610d9f8cc3ac869731be4e2de869
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-05 10:23:00 -0300

    chore: created route Home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header'
+import InputText from '@/components/InputTask'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <InputText />
+    </>
+  )
+}
