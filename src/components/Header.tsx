9e9c006c1a8afa8fd2589946e9447fecc31fe99a
Gildemberg Leite <gildembergleite@gmail.com>
2023-11-05 10:23:18 -0300

    feat: created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="w-full">
+      <div className="flex w-full h-52 bg-gray-700 justify-center items-center">
+        <h1 className="text-5xl">
+          <span className="text-blue font-black">to</span>
+          <span className="text-purple-dark font-black">do</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
